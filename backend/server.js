35727b721510929673cc71aec98a26bda3fb3681
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2022-07-27 19:03:43 +0530

    ready for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())  // to parse body in json format (body parser)
 app.use(express.urlencoded({extended:true}))
 const PORT= process.env.PORT || 5000
-const uri  = "mongodb+srv://Print-X:Pass%40123@cluster0.w844m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri  = "mongodb://Jaydeep-shelake:Pass%40123@cluster0-shard-00-00.dvyoz.mongodb.net:27017,cluster0-shard-00-01.dvyoz.mongodb.net:27017,cluster0-shard-00-02.dvyoz.mongodb.net:27017/?ssl=true&replicaSet=atlas-wt8c8d-shard-0&authSource=admin&retryWrites=true&w=majority";
 //Pass%40123
 mongoose.connect(uri,
     err => {
@@ -22,25 +22,25 @@ mongoose.connect(uri,
 // app.use(cors())
 
 //place all routes here
-app.use('/',billRouter)
+app.use('/api',billRouter)
 
 //production
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 if(process.env.NODE_ENV==='production'){
     //set a static folder
-    app.get('*', (req, res) =>{
-      res.sendFile(
-        path.resolve(__dirname, '../frontend', 'build', 'index.html')
-      )
-     } );
+    app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../frontend', 'build', 'index.html')
+    )
+  );
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
 }
-
 
 //listen
 app.use((err,req,res,next)=>{
